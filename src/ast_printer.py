bcc22c7316cd236ab0c69ecdd8f0b0ecd38251b5
AlbertUlysses <albertulysseschavez@gmail.com>
2026-01-30 22:26:59 -0800

    fix the print ast function

--- src/ast_printer.py
+++ src/ast_printer.py
@@ -8,18 +8,7 @@ from token_type import TokenType
 class ASTPrinter:
 
     def print_ast(self, expr: Expr) -> str:
-        ending_int = expr.__doc__.find('(')
-        match expr.__doc__[:ending_int]:
-            case 'Binary':
-                return self.visit_binary_expr(expr)
-            case 'Grouping':
-                return self.visit_grouping_expr(expr)
-            case 'Literal':
-                return self.visit_literal_expr(expr)
-            case 'Unary':
-                return self.visit_unary_expr(expr)
-            case _:
-                pass
+        return expr.accept(self)
 
     def parenthesize(self, name: str, exprs_values: list[str]) -> str:
         return " ".join(["("] + [name] + [str(expr.accept(self)) for expr in exprs_values]+[")"])
